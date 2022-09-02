b679d59e429cf5d34d8e6c44d033c0b3c7355040
Morel Sébastien <morel.seb@gmail.com>
2022-09-01 21:43:34 -0700

    feat(cli): Better design for progression bar

--- components/crystallize-cli/src/core/components/ImportStatus.tsx
+++ components/crystallize-cli/src/core/components/ImportStatus.tsx
@@ -1,25 +1,25 @@
 import { Box, Text } from 'ink';
 import React from 'react';
-//@ts-ignore
-import ProgressBar from 'ink-progress-bar';
+import { colors } from '../../config/colors.js';
 
-const areaMap = new Map();
-areaMap.set('media', 'Media');
-areaMap.set('shapes', 'Shapes');
-areaMap.set('grids', 'Grids');
-areaMap.set('items', 'Items');
-areaMap.set('languages', 'Languages');
-areaMap.set('priceVariants', 'Price variants');
-areaMap.set('vatTypes', 'VAT types');
-areaMap.set('topicMaps', 'Topic maps');
-areaMap.set('customers', 'Customers');
-areaMap.set('orders', 'Orders');
+const areaMap = {
+    media: 'Media',
+    shapes: 'Shapes',
+    grids: 'Grids',
+    items: 'Items',
+    languages: 'Languages',
+    priceVariants: 'Price variants',
+    vatTypes: 'VAT types',
+    topicMaps: 'Topic maps',
+    customers: 'Customers',
+    orders: 'Orders',
+};
 
 function progressText(progress: number) {
     const arr: string[] = Array.apply(null, Array(20)).map((_) => '-');
     const filled: string[] = Array.apply(null, Array(Math.floor(progress * 20))).map((_) => '=');
     arr.splice(0, filled.length, ...filled);
-    return `[${arr.join('')}]`;
+    return arr.join('');
 }
 
 type Status = Record<
@@ -34,13 +34,25 @@ export const ImportStatus: React.FC<{ status: Status }> = ({ status }) => {
     return (
         <>
             {Object.keys(status).map((key: string) => {
-                const name = areaMap.get(key) || key;
+                const name = areaMap[key as keyof typeof areaMap] || key;
                 const { progress, warnings } = status[key];
                 return (
                     <React.Fragment key={key}>
                         <Box flexDirection="column">
                             <Text>
-                                {progressText(progress)} | {(progress * 100).toFixed(0).padStart(3)}% | {name}
+                                <Text bold color={colors.warning}>
+                                    [
+                                </Text>
+                                <Text dimColor>{progressText(progress)}</Text>
+                                <Text bold color={colors.warning}>
+                                    ]
+                                </Text>{' '}
+                                {(progress * 100).toFixed(0).padStart(3)}%{' '}
+                                <Text bold color={colors.info}>
+                                    {' '}
+                                    |
+                                </Text>{' '}
+                                {name}
                             </Text>
                         </Box>
                         {warnings.map((warn: { message: string; code: number }, index: number) => (
