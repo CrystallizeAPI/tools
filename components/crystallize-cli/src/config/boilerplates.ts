1d8608698561ceec676b67430c621b2c94f99620
Morel Sébastien <morel.seb@gmail.com>
2023-06-06 12:30:53 -0700

    feat(crystallize-cli-next): introducing Next.js boilerplate

--- components/crystallize-cli/src/config/boilerplates.ts
+++ components/crystallize-cli/src/config/boilerplates.ts
@@ -3,11 +3,20 @@ import { Boilerplate } from '../types';
 export const boilerplates: Boilerplate[] = [
     {
         identifier: 'furniture-remix',
-        name: 'Furniture Remix',
-        baseline: 'NEW!: Complete ecommerce using Remix Run',
-        description: 'React, SSR, Ecommerce with basket & checkout, promotions, Stripe, etc.',
+        name: 'Furniture Remix Run',
+        baseline: 'Complete ecommerce using Remix Run',
+        description: 'React, SSR, Ecommerce with basket & checkout, promotions, Payments (many), etc.',
         demo: 'https://furniture.superfast.store',
         blueprint: 'frntr-blueprint',
         git: 'https://github.com/CrystallizeAPI/furniture-remix',
+    },
+    {
+        identifier: 'furniture-nextjs',
+        name: 'Furniture Next.js',
+        baseline: 'Complete ecommerce using Next.js',
+        description: 'React, SSR, Ecommerce with basket & checkout, promotions, Payments (many), etc.',
+        demo: 'https://furniture.superfast.store',
+        blueprint: 'frntr-blueprint',
+        git: 'https://github.com/CrystallizeAPI/furniture-nextjs',
     },
 ];
