bcdfb6fabe9a6c0323db35cf2af1e9d4b82b5e92
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-02-13 16:45:00 +0200

    feat: add furnitut boilerplate config

--- components/cli/src/content/boilerplates.ts
+++ components/cli/src/content/boilerplates.ts
@@ -2,6 +2,15 @@ import type { Boilerplate } from '../domain/contracts/models/boilerplate';
 
 export const boilerplates: Boilerplate[] = [
     {
+        identifier: 'nextjs-furnitut-boilerplate',
+        name: 'Furnitut Next.js Boilerplate',
+        baseline: 'Complete ecommerce using Next.js',
+        description: 'React, SSR, Ecommerce with basket & checkout, payments, etc.',
+        demo: 'https://www.furnitut.com/',
+        blueprint: 'furnitut',
+        git: 'https://github.com/CrystallizeAPI/nextjs-furnitut',
+    },
+    {
         identifier: 'nerd-factory-boilerplate',
         name: 'Nerd Factory Boilerplate',
         baseline: 'The subcscrition accelerator built with Next.js 15',
