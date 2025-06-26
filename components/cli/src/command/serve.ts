4d4e0963ce77c62cac16915602e9f82aea199b4a
Morel Sébastien <morel.seb@gmail.com>
2025-06-26 16:07:30 +0200

    feat(cli): make the enroll tenant a GET to simplify frontend call with SSE

--- components/cli/src/command/serve.ts
+++ components/cli/src/command/serve.ts
@@ -20,98 +20,94 @@ export const createServeCommand = ({ logger, commandBus, feedbackPiper }: Deps):
                 const server = Bun.serve({
                     idleTimeout: 255,
                     port: 3000,
-                    fetch: async (req: Request) => {
-                        if (req.method !== 'POST') {
-                            return new Response('Crystallize CLI - Web Server');
-                        }
-                        const post = await req.json();
-                        switch (post.command) {
-                            case 'enroll-tenant':
-                                if (!post.tenantIdentifier) {
-                                    return new Response('Missing tenantIdentifier', { status: 400 });
-                                }
-                                if (!post.boilerplate) {
-                                    return new Response('Missing boilerplate', { status: 400 });
-                                }
-                                const boilerplate: Boilerplate | undefined = boilerplates.find(
-                                    (b) => b.identifier === post.boilerplate,
-                                );
-                                if (!boilerplate) {
-                                    return new Response('Missing boilerplate', { status: 400 });
-                                }
-                                const sessionId = req.headers.get('cookie')?.match(/connect\.sid=([^;]+)/)?.[1];
-                                if (!sessionId) {
-                                    if (!post.credentials) {
-                                        return new Response('Missing credentials', { status: 400 });
-                                    }
+                    routes: {
+                        '/enroll-tenant/:tenantIdentifier/:boiler': (req) => {
+                            const tenantIdentifier = req.params.tenantIdentifier;
+                            const boiler = req.params.boiler;
+                            const url = new URL(req.url);
+                            const doIgnite = url.searchParams.get('ignite') === 'true';
+                            const boilerplate: Boilerplate | undefined = boilerplates.find(
+                                (b) => b.identifier === boiler,
+                            );
+                            if (!boilerplate) {
+                                return new Response('Missing boilerplate', { status: 400 });
+                            }
+                            const sessionId = req.headers.get('cookie')?.match(/connect\.sid=([^;]+)/)?.[1];
+                            const tokenId = req.headers.get('X-Crystallize-Access-Token-Id');
+                            const tokenSecret = req.headers.get('X-Crystallize-Access-Token-Secret');
+                            if (!sessionId) {
+                                if (!tokenId || !tokenSecret) {
                                     return new Response('Missing credentials', { status: 400 });
                                 }
-                                const stream = new ReadableStream({
-                                    async start(controller) {
-                                        const send = (feedback: FeedbackMessage) => {
-                                            controller.enqueue(`data: ${JSON.stringify(feedback)}\n\n`);
-                                        };
-                                        const pingInterval = setInterval(() => {
-                                            controller.enqueue(`data: ping\n\n`);
-                                        }, 3000);
-
-                                        feedbackPiper.subscribe(send);
-                                        send({
-                                            level: 'info',
-                                            message: `Enrolling tenant ${post.tenantIdentifier} with boilerplate ${boilerplate.name} started.`,
-                                        });
+                            }
+                            const stream = new ReadableStream({
+                                async start(controller) {
+                                    const send = (feedback: FeedbackMessage) => {
                                         try {
-                                            const intent = commandBus.createCommand(
-                                                'EnrollTenantWithBoilerplatePackage',
-                                                {
-                                                    tenantIdentifier: post.tenantIdentifier,
-                                                    credentials: sessionId
-                                                        ? {
-                                                              sessionId,
-                                                          }
-                                                        : {
-                                                              ACCESS_TOKEN_ID: post.credentials.ACCESS_TOKEN_ID,
-                                                              ACCESS_TOKEN_SECRET: post.credentials.ACCESS_TOKEN_SECRET,
-                                                          },
-                                                    boilerplate,
-                                                    doIgnite: post.ignite || false,
-                                                },
-                                            );
-                                            const { result } = await commandBus.dispatch(intent);
+                                            controller.enqueue(`data: ${JSON.stringify(feedback)}\n\n`);
+                                        } catch {}
+                                    };
+                                    const pingInterval = setInterval(() => {
+                                        try {
+                                            controller.enqueue(`data: ping\n\n`);
+                                        } catch {}
+                                    }, 3000);
 
-                                            if (!result) {
-                                                send({
-                                                    level: 'error',
-                                                    message: `Enrolling tenant ${post.tenantIdentifier} with boilerplate ${boilerplate.name} failed.`,
-                                                });
-                                            }
+                                    feedbackPiper.subscribe(send);
+                                    send({
+                                        level: 'info',
+                                        message: `Enrolling tenant ${tenantIdentifier} with boilerplate ${boilerplate.name} started.`,
+                                    });
+                                    try {
+                                        const intent = commandBus.createCommand('EnrollTenantWithBoilerplatePackage', {
+                                            tenantIdentifier: tenantIdentifier,
+                                            credentials: sessionId
+                                                ? {
+                                                      sessionId,
+                                                  }
+                                                : {
+                                                      ACCESS_TOKEN_ID: tokenId || '',
+                                                      ACCESS_TOKEN_SECRET: tokenSecret || '',
+                                                  },
+                                            boilerplate,
+                                            doIgnite: doIgnite || false,
+                                        });
+                                        const { result } = await commandBus.dispatch(intent);
 
-                                            send({
-                                                level: 'info',
-                                                message: `Enrolling tenant ${post.tenantIdentifier} with boilerplate ${boilerplate.name} done!`,
-                                            });
-                                        } catch (err) {
+                                        if (!result) {
                                             send({
                                                 level: 'error',
-                                                message: `${(err as Error).message}`,
+                                                message: `Enrolling tenant ${tenantIdentifier} with boilerplate ${boilerplate.name} failed.`,
                                             });
-                                        } finally {
-                                            clearInterval(pingInterval);
-                                            feedbackPiper.unsubscribe(send);
-                                            controller.close();
                                         }
-                                    },
-                                });
-                                return new Response(stream, {
-                                    headers: {
-                                        'Content-Type': 'text/event-stream',
-                                        'Cache-Control': 'no-cache',
-                                        Connection: 'keep-alive',
-                                    },
-                                });
-                            default:
-                                return new Response(`Unknown command: ${post.command}`, { status: 400 });
-                        }
+
+                                        send({
+                                            level: 'info',
+                                            message: `Enrolling tenant ${tenantIdentifier} with boilerplate ${boilerplate.name} done!`,
+                                        });
+                                    } catch (err) {
+                                        send({
+                                            level: 'error',
+                                            message: `${(err as Error).message}`,
+                                        });
+                                    } finally {
+                                        clearInterval(pingInterval);
+                                        feedbackPiper.unsubscribe(send);
+                                        controller.close();
+                                    }
+                                },
+                            });
+                            return new Response(stream, {
+                                headers: {
+                                    'Content-Type': 'text/event-stream',
+                                    'Cache-Control': 'no-cache',
+                                    Connection: 'keep-alive',
+                                },
+                            });
+                        },
+                    },
+                    fetch: () => {
+                        return new Response('Crystallize CLI - Web Server');
                     },
                 });
                 logger.info(`Server running on http://localhost:${server.port}`);
